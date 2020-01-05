452d45aedc7d2618cab1ec7048739dc0d2a9d80a
bhutton <ben@benhutton.com.au>
2020-01-05 21:59:47 +0800

    Added test for displayMessage.

--- src/com/pingpong/tests/TestMessages.java
+++ src/com/pingpong/tests/TestMessages.java
@@ -2,8 +2,9 @@ package com.pingpong.tests;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.File;
@@ -16,20 +17,17 @@ import org.junit.Test;
 
 public class TestMessages {
 	
-	private final Messages msg = new Messages();
-	private final Graphics g = mock(Graphics.class);
+	private final Messages messages = new Messages();
+	private final Graphics graphicsMock = mock(Graphics.class);
 	
 	@Test
 	public void gameOverImg() throws IOException {
 		String current = new java.io.File( "." ).getCanonicalPath();
 		String gameOverImage = current + "/src/com/pingpong/images/game-over-message.png";
-		BufferedImage imgGameOver1 = null, imgGameOver2 = null;
+		BufferedImage imgGameOver1 = ImageIO.read(new File(gameOverImage)), imgGameOver2;
 		
-		try { imgGameOver1 = ImageIO.read(new File(gameOverImage)); } 
-		catch (IOException e) { e.printStackTrace(); }
-		
-		msg.setGameOverImage(gameOverImage);
-		imgGameOver2 = msg.getGameOverImage(); 
+		messages.setGameOverImage(gameOverImage);
+		imgGameOver2 = messages.getGameOverImage();
 		
 		byte[] byteArray1 = ((DataBufferByte) imgGameOver1.getData().getDataBuffer()).getData();
 		byte[] byteArray2 = ((DataBufferByte) imgGameOver2.getData().getDataBuffer()).getData();
@@ -39,41 +37,53 @@ public class TestMessages {
 	
 	@Test
 	public void testSetGameActive() {
-		assertEquals(true, msg.setGameActive());
+		assertEquals(true, messages.setGameActive());
 	}
 	
 	@Test
 	public void testGetGameActive() {
-		assertEquals(false, msg.getGameActive());
+		assertEquals(false, messages.getGameActive());
 	}
 	
 	@Test
 	public void testSetGameOver() {
-		assertEquals(true, msg.setGameActive());
-		assertEquals(false, msg.setGameOver(1,1));
-		assertEquals(false, msg.getGameActive());
+		assertEquals(true, messages.setGameActive());
+		assertEquals(false, messages.setGameOver(1,1));
+		assertEquals(false, messages.getGameActive());
 	}
 
 	@Test
 	public void testReturnMessage() {
-		assertEquals("Press Enter to Try Again", msg.returnMessage());
+		assertEquals("Press Enter to Try Again", messages.returnMessage());
 	}
 	
 	@Test
 	public void testDisplayGameStatsAtBottomOfScreen() {
-		msg.displayGameStatsAtBottomOfScreen(g, 800, 600);
+		messages.displayGameStatsAtBottomOfScreen(graphicsMock, 800, 600);
 	}
 	
 	@Test
 	public void testSetAndGetLevel() {
-		msg.setLevel(2);
-		assertEquals(2, msg.getLevel());
+		messages.setLevel(2);
+		assertEquals(2, messages.getLevel());
 	}
 	
 	@Test
 	public void testSetAndGetLives() {
-		msg.setLives(2);
-		assertEquals(2, msg.getLives());
+		messages.setLives(2);
+		assertEquals(2, messages.getLives());
 	}
 
+	@Test
+	public void displayMessage() {
+		int xStartMessage = -70;
+		int yStartMessage = 60;
+		int xGameOver = 0;
+		int yGameOver = 0;
+
+		messages.displayMessage(graphicsMock, 20, 20, 20, 20);
+		verify(graphicsMock).setColor(Color.GRAY);
+		verify(graphicsMock).drawImage(null, xGameOver, yGameOver, null);
+		verify(graphicsMock).drawString(messages.returnMessage(), xStartMessage, yStartMessage);
+	}
 }
